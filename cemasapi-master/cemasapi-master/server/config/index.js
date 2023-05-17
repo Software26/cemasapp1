a526dc705d66bd280ab2124e47c494c71b324c4c
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 23:17:17 -0400

    Create index.js

---
+++ cemasapi-master/cemasapi-master/server/config/index.js
@@ -0,0 +1,24 @@
+//Puerto
+const dotenv = require('dotenv');
+dotenv.config();
+
+process.env.PORT = process.env.PORT;
+const USER = process.env.MONGO_USER;
+const PASS = process.env.MONGO_PASS;
+
+//Entorno
+
+const NODE_ENV = process.env.NODE_ENV;
+
+let urlDB;
+
+if (NODE_ENV === 'dev') {
+	//urlDB = `mongodb://${USER}:${PASS}@localhost/cemas`;
+
+	urlDB = `mongodb+srv://${USER}:${PASS}@cluster0-4fmpq.mongodb.net/cemas?retryWrites=true&w=majority`;
+
+} else {
+	urlDB = `mongodb+srv://${USER}:${PASS}@cluster0-4fmpq.mongodb.net/cemas?retryWrites=true&w=majority`;
+}
+
+process.env.URLDB = urlDB;
