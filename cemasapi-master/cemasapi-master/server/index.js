b6c640d37ca76bc17ab311ec645a868508270e24
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 23:17:34 -0400

    Create index.js

---
+++ cemasapi-master/cemasapi-master/server/index.js
@@ -0,0 +1,52 @@
+require('./config');
+﻿require('rootpath')();
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const boddyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+const URLDB = process.env.URLDB;
+const cors = require('cors');
+const jwt = require('./_helpers/jwt');
+const errorHandler = require('./_helpers/error-handler');
+
+app.use(boddyParser.urlencoded({ extended: false }));
+app.use(boddyParser.json());
+app.use(cors());
+
+//configuracion goblar de rutas
+app.use(require('./routes'));
+app.use(fileUpload());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+//app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+mongoose.set('useFindAndModify', false);
+
+
+console.log(URLDB, PORT)
+mongoose
+	.connect(
+		URLDB,
+		{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+		(err, res) => {
+			if (err) throw err;
+			console.log('Mongo online');
+		}
+	)
+	.then(() => console.log('DB Connected!'))
+	.catch((err) => {
+		console.log(Error, 'No sirve');
+	});
+
+app.listen(PORT, () => {
+	console.log('Escuchando puerto', PORT);
+});
