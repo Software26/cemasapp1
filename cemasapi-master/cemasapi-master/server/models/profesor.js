322ab42d74fb8d87656cf77770fd7b0a1b6f5464
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-16 23:17:48 -0400

    Create profesor.js

---
+++ cemasapi-master/cemasapi-master/server/models/profesor.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let profesorSchema = new Schema({
+	_id: Schema.Types.ObjectId,
+	cursos_impartidos: [
+		{
+			type: String
+		}
+	],
+	asignaturas_impartidas: [
+		{
+			type: String
+		}
+	],
+	nombre: {
+		type: String
+	},
+	apellido: {
+		type: String
+	},
+	codigo_maestro: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	img: {
+		type: String
+	},
+	telefono: {
+		type: String
+	},
+	estado: {
+		type: String
+	},
+
+	updated: {
+		type: Date,
+		default: Date.now
+	},
+	edad: {
+		type: Number,
+		min: 6,
+		max: 60
+	}
+});
+
+module.exports = mongoose.model('Profesor', profesorSchema);
